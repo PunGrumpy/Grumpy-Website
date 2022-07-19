06339a27b84c9306938fbc303ff1e6e300af5d5b
Noppakorn Kaewsalabnil <108584943+PunGrumpy@users.noreply.github.com>
2022-07-19 21:06:04 +0700

    Add files via upload

---
+++ components/layouts/main.js
@@ -0,0 +1,54 @@
+import Head from 'next/head'
+import dynamic from 'next/dynamic'
+import NavBar from '../navbar'
+import { Box, Container } from '@chakra-ui/react'
+import Footer from '../footer'
+import VoxelDogLoader from '../voxel-ghost-loader'
+
+const LazyVoxelDog = dynamic(() => import('../voxel-ghost'), {
+  ssr: false,
+  loading: () => <VoxelDogLoader />
+})
+
+const Main = ({ children, router }) => {
+  return (
+    <Box as="main" pb={8}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Grumpy's Website" />
+        <meta name="author" content="Noppakorn Kaewsalabnil" />
+        <meta name="author" content="Grumpy" />
+        <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <meta name="twitter:title" content="Noppakorn Kaewsalabnul" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@grumpy" />
+        <meta name="twitter:creator" content="@grumpy" />
+        <meta
+          name="twitter:image"
+          content="https://github.com/PunGrumpy/Grumpy-Website/blob/main/public/card.png?raw=true"
+        />
+        <meta property="og:site_name" content="Noppakorn Kaewsalabnil" />
+        <meta name="og:title" content="Noppakorn Kaewsalabnil" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://github.com/PunGrumpy/Grumpy-Website/blob/main/public/card.png?raw=true"
+        />
+        <title>Grumpy - Website</title>
+      </Head>
+
+      <NavBar path={router.asPath} />
+
+      <Container maxW="container.md" pt={14}>
+        <LazyVoxelDog />
+
+        {children}
+
+        <Footer />
+      </Container>
+    </Box>
+  )
+}
+
+export default Main
---
+++ pages/index.js
@@ -0,0 +1,183 @@
+import {
+  Link,
+  Container,
+  Heading,
+  Box,
+  Button,
+  List,
+  ListItem,
+  useColorModeValue,
+  chakra
+} from '@chakra-ui/react'
+import Paragraph from '../components/paragraph'
+import { BioSection, BioYear } from '../components/bio'
+import Layout from '../components/layouts/article'
+import Section from '../components/section'
+import {
+  IoLogoDiscord,
+  IoLogoInstagram,
+  IoLogoGithub,
+  IoLogoLinkedin
+} from 'react-icons/io5'
+import Image from 'next/image'
+
+const ProfileImage = chakra(Image, {
+  shouldForwardProp: prop => ['width', 'height', 'src', 'alt'].includes(prop)
+})
+
+const Home = () => (
+  <Layout>
+    <Container>
+      <Box
+        borderRadius="lg"
+        mb={6}
+        p={3}
+        textAlign="center"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        css={{ backdropFilter: 'blur(10px)' }}
+      >
+        Hello, I&apos;m a student computer science based in Thailand!
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Noppakorn Kaewsalabnil
+          </Heading>
+          <p> Undergraduate student - Computer Science </p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign="center"
+        >
+          <Box
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            w="100px"
+            h="100px"
+            display="inline-block"
+            borderRadius="full"
+            overflow="hidden"
+          >
+            <ProfileImage
+              src="/images/grumpy.jpg"
+              alt="Profile image"
+              borderRadius="full"
+              width="100%"
+              height="100%"
+            />
+          </Box>
+        </Box>
+      </Box>
+
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>2002</BioYear>
+          Born in Bangkok, Thailand
+        </BioSection>
+        <BioSection>
+          <BioYear>2020</BioYear>
+          Graduated from high school at{' '}
+          <Link href="http://www.sriayudhya.ac.th/" target="_blank">
+            Sriayudhya School
+          </Link>
+        </BioSection>
+        <BioSection>
+          <BioYear>2021</BioYear>
+          Start learning to code
+        </BioSection>
+        <BioSection>
+          <BioYear>Present</BioYear>
+          Study at 1st -{' '}
+          <Link href="https://www.kmitl.ac.th/" target="_blank">
+            King Mongkut&apos;s Institute of Technology Ladkrabang
+          </Link>
+        </BioSection>
+      </Section>
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          I Love
+        </Heading>
+        <Paragraph>
+          Coding, Music,{' '}
+          <Link href="https://www.instagram.com/ansonseabra/" target="_blank">
+            Singer
+          </Link>
+          , Playing Guitar,{' '}
+          <Link href="https://500px.com/p/whatthepunphoto" target="_blank">
+            Photography
+          </Link>
+          , Custom Keyboard, Machine Learning
+        </Paragraph>
+      </Section>
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          On the web
+        </Heading>
+        <List>
+          <ListItem>
+            <Link href="https://github.com/PunGrumpy" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoGithub />}
+              >
+                @PunGrumpy
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link
+              href="https://discordapp.com/users/353899973252874260"
+              target="_blank"
+            >
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoDiscord />}
+              >
+                @Grumpy#9760
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://www.instagram.com/wtpp_p114/" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoInstagram />}
+              >
+                @wtpp_p114
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link
+              href="https://www.linkedin.com/in/noppakorn-kaewsalabnil/"
+              target="_blank"
+            >
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoLinkedin />}
+              >
+                @Noppakorn Kaewsalabnil
+              </Button>
+            </Link>
+          </ListItem>
+        </List>
+      </Section>
+    </Container>
+  </Layout>
+)
+
+export default Home
+export { getServerSideProps } from '../components/chakra'
